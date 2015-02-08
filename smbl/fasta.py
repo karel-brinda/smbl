2ac1420e63c8fe370c7378da438d367f3373e9ae
Karel Brinda <karel.brinda@gmail.com>
2015-02-08 23:26:47 +0100

    Small update

--- smbl/fasta.py
+++ smbl/fasta.py
@@ -1,11 +1,18 @@
 import os
 import smbl
 
-EXAMPLE                             = os.path.join(smbl.fa_dir,"example_fasta.fa")
+EXAMPLE_1 = EXAMPLE                 = os.path.join(smbl.fa_dir,"example_fasta_1.fa")
+EXAMPLE_2                           = os.path.join(smbl.fa_dir,"example_fasta_2.fa")
+EXAMPLE_3                           = os.path.join(smbl.fa_dir,"example_fasta_3.fa")
+
+
+
 HUMAN_HG38 = HUMAN_GRCH38           = os.path.join(smbl.fa_dir,"hg38.fa")
 
 FAS = [
-		EXAMPLE,
+		EXAMPLE_1,
+		EXAMPLE_2,
+		EXAMPLE_3,
 		HUMAN_HG38
 	]
 
