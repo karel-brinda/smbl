d10657a8fa0525dd6cbf75a61a18c254a2a8b99d
Karel Břinda <karel.brinda@gmail.com>
2015-03-20 18:58:10 +0100

    Single thread Travis test

--- tests/pytest_plugins.py
+++ tests/pytest_plugins.py
@@ -1,10 +1,14 @@
 import pytest
 import smbl
 
+# todo: ensure that cmake is installed
+
+smbl.prog.CMake.install_all_steps()
+
 @pytest.mark.parametrize("plugin,plugin_name", 
 		[
 			(x,x.get_plugin_name())
-			for x in smbl.prog.plugins.get_registered_plugins() if x.is_platform_supported()
+			for x in smbl.prog.plugins.get_registered_plugins() if x.is_platform_supported() and x is not smbl.prog.CMake
 		]
 	)
 def test_plugins(plugin,plugin_name):
