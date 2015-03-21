fbf4c093e169d176d05824fbb2d8fc38cdb3d54b
Karel Břinda <karel.brinda@gmail.com>
2015-03-21 11:01:46 +0100

    Ambiguity bug -- ambiguous rules are detected and registered only once

--- smbl/prog/plugins/_program.py
+++ smbl/prog/plugins/_program.py
@@ -15,7 +15,9 @@ def get_registered_plugins():
 	return sorted(list(__PLUGINS),key=lambda x:x.get_plugin_name())
 
 def register_rule(rule):
-	__RULES.add(rule)
+	encodings=[r.encode() for r in get_registered_rules()]
+	if rule.encode() not in encodings:
+		__RULES.add(rule)
 
 def get_registered_rules():
 	return list(__RULES)
@@ -211,11 +213,11 @@ class Program(metaclass=ProgramWatcher):
 
 class Rule:
 	def __init__(self,input,output,run):
-		register_rule(self)
-
 		self.__input=input
 		self.__output=output
 		self.__run=run
+
+		register_rule(self)
 
 	def get_input(self):
 		return self.__input
@@ -226,6 +228,5 @@ class Rule:
 	def run(self):
 		self.__run()
 
-	def __hash__(self):
-		return "{} {}".format(str(input),str(input))
-
+	def encode(self):
+		return "{} {}".format(str(self.__input),str(self.__output))
