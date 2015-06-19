a23035ccf0e0af5bf2a036871491ba80f9854ad7
Karel Břinda <karel.brinda@gmail.com>
2015-06-19 22:31:46 +0200

    setup.py check

--- install.sh
+++ install.sh
@@ -4,4 +4,19 @@ set -e
 
 cd "$(dirname "$0")"
 rm -fR build dist SMBL.egg-info
+
+echo
+echo
+echo "   TEST"
+echo
+echo
+
+python3 setup.py check
+
+echo
+echo
+echo "   INSTALLATION"
+echo
+echo
+
 python3 setup.py install
